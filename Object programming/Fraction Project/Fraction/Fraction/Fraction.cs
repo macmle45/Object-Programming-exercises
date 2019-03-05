0558a025b0da6be0957f34a57e7b19aba53437b1
macowsky <maciej.mleczko.net@gmail.com>
2019-03-05 22:55:18 +0100

    added constructor and static fields with representations of four fractions

--- Object programming/Fraction Project/Fraction/Fraction/Fraction.cs
+++ Object programming/Fraction Project/Fraction/Fraction/Fraction.cs
@@ -8,6 +8,23 @@ namespace Fraction
 {
     public struct Fraction
     {
-        private int numerator, denominator;
+        //private fields
+        private int numerator;
+        private int denominator;
+
+        //constructor with default argument
+        public Fraction(int numerator, int denominator = 1)
+        {
+            if (denominator == 0)
+                throw new ArgumentException("Denominator must be greater than zero");
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        //instances of Fraction struct
+        public static readonly Fraction Zero = new Fraction(0);
+        public static readonly Fraction One = new Fraction(1);
+        public static readonly Fraction Half = new Fraction(1, 2);
+        public static readonly Fraction Quarter = new Fraction(1, 4);
     }
 }
