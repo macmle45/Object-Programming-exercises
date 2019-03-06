883024f84bc37153b2a1343b5086911eaf2c8fc6
macowsky <maciej.mleczko.net@gmail.com>
2019-03-06 13:08:53 +0100

    modified plus operator method

--- Object programming/Fraction Project/Fraction/Fraction/Program.cs
+++ Object programming/Fraction Project/Fraction/Fraction/Program.cs
@@ -14,13 +14,15 @@ namespace Fraction
             Fraction f2 = new Fraction(22, 7);
             Fraction f3 = new Fraction(8, 24);
 
-            f3.SimplifyFraction();
 
-            Console.WriteLine(f1.ToString());
-            Console.WriteLine(f2.ToDouble());
-            Console.WriteLine(f3.ToString());
 
-            Console.WriteLine((f2 - f3).ToString());
+            Console.WriteLine($"Operations on two fraction: {f2}, {f3}\n");
+
+            Console.WriteLine($"Negative:\t{(-f2).ToString()} or {(-f3).ToString()}");
+            Console.WriteLine($"Minus:\t\t{(f2 - f3).ToString()}");
+            Console.WriteLine($"Plus:\t\t{(f2 + f3).ToString()}");
+            Console.WriteLine($"Multiplication:\t{(f2 * f3).ToString()}");
+            Console.WriteLine($"Divide:\t\t{(f2 / f3).ToString()}");
 
 
             Console.WriteLine($"\n\n\n>>>\t{Fraction.Info()}\t<<<");
