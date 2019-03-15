4bb0165ea377acab2e684dd8feac122ca613ec31
macowsky <maciej.mleczko.net@gmail.com>
2019-03-15 13:59:09 +0100

    added security when Time struct is created | added override Equals method | implemented IEquatable<> interface

--- Object programming/Time Period Project/TimePeriod/TimePeriod Logic/Time.cs
+++ Object programming/Time Period Project/TimePeriod/TimePeriod Logic/Time.cs
@@ -16,9 +16,16 @@ namespace TimePeriod_Logic
         //basic constructor
         public Time(byte hh, byte mm, byte ss)
         {
-            this.hours = hh;
-            this.minutes = mm;
-            this.seconds = ss;
+            if(hh >=24 || mm >= 60 || ss >= 60)
+            {
+                throw new ArgumentOutOfRangeException("Argument out of range.");
+            }
+            else
+            {
+                this.hours = hh;
+                this.minutes = mm;
+                this.seconds = ss;
+            }
         }
 
         //constructor for two-parameter option
@@ -30,9 +37,22 @@ namespace TimePeriod_Logic
         //constructor for string-parameter option ("hh:mm:ss")
         public Time(string t)
         {
-            this.hours = byte.Parse(t.Substring(0, 2));
-            this.minutes = byte.Parse(t.Substring(3, 2));
-            this.seconds = byte.Parse(t.Substring(6, 2));
+            if (t.Length > 8)
+                throw new ArgumentException("Invalid argument. Correct argument: 'hh:mm:ss'.");
+            else
+            {
+                try
+                {
+                    this.hours = byte.Parse(t.Substring(0, 2));
+                    this.minutes = byte.Parse(t.Substring(3, 2));
+                    this.seconds = byte.Parse(t.Substring(6, 2));
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            
         }
 
         public override string ToString()
@@ -40,16 +60,17 @@ namespace TimePeriod_Logic
             return $"{Hours}:{Minutes}:{Seconds}";
         }
 
-        public override bool Equals(object obj)
+        public override bool Equals(object t)
         {
-            return base.Equals(obj);
+            if (t == null || this.GetType() != t.GetType()) return false;
+
+            return (this.Hours == ((Time)t).Hours && this.Minutes == ((Time)t).Minutes && this.Seconds == ((Time)t).Seconds);
         }
 
         #region interfaces implementation
         public bool Equals(Time t)
         {
-            
-            return true;
+            return this.Equals(t);
         }
 
         public int CompareTo(Time t)
@@ -59,6 +80,17 @@ namespace TimePeriod_Logic
         #endregion
 
         #region overriding operators
+
+        public static bool operator ==(Time t1, Time t2)
+        {
+            return t1.Equals(t2);
+        }
+
+        public static bool operator !=(Time t1, Time t2)
+        {
+            return !t1.Equals(t2);
+        }
+
         #endregion
 
 
